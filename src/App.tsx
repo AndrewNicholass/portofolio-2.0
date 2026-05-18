69e51a2c97214a8e3041e6bd857c4991ea4fa87a
Andrew Nicholas <andrew.nic3@gmail.com>
2026-05-18 14:32:16 +0700

    fix: build stuff 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import Navbar from './components/navbar.jsx'
-import Hero from './components/hero.jsx'
-import Projects from './components/projects.jsx'
-import Experience from './components/experience.jsx'
-import Contact from './components/contact.jsx'
-import ClickSpark from './components/clickSpark.jsx'
+import Navbar from './components/navbar';
+import Hero from './components/hero';
+import Projects from './components/projects';
+import Experience from './components/experience';
+import Contact from './components/contact';
+import ClickSpark from './components/ClickSpark';
 // import Lanyard from './components/lanyard.jsx'
-import LogoLoop from './components/LogoLoop.jsx'
+import LogoLoop from './components/LogoLoop';
 import { SiReact, SiNextdotjs, SiTypescript, SiTailwindcss } from 'react-icons/si';
 
 const techLogos = [
---
+++ src/types/global.d.ts
@@ -0,0 +1,1 @@
+declare module './components/navbar.jsx'
